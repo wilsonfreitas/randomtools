f28311e647bf67f56e45acb6b6761b56f53e1f0c
Wilson Nascimento de Freitas <wilson.freitas@gmail.com>
2013-04-20 10:56:49 -0300

    organizing tests

--- test.py
+++ test.py
@@ -34,141 +34,113 @@ class Test(unittest.TestCase):
         return 1 - stats.chi2(2).cdf(jb)
 
     def test_t_uniform(self):
-        rand = rt.uniform(self.rand, 0.0, 1.0)
-        t = self.t_pvalue(rand, 0.5)
-        self.assert_(t > 0.05,
-            'mean test for uniform random number generator: %f' % t)
         rand = rt.uniform(self.rand, -1, 1)
         t = self.t_pvalue(rand)
-        self.assert_(t > 0.05,
-            'mean test for uniform random number generator: %f' % t)
+        self.assertGreaterEqual(t, 0.05, 'mean test for uniform RNG: %f' % t)
 
     def test_t_marsaglia(self):
         rand = rt.marsaglia(self.rand)
         t = self.t_pvalue(rand)
-        self.assertEqual(t > 0.05, True, 
-            'mean test for gaussian random number generator (Marsaglia): %f'
-            % t)
+        self.assertGreaterEqual(t, 0.05, 
+            'mean test for gaussian RNG (Marsaglia): %f' % t)
 
     def test_t_moro(self):
         rand = rt.moro(self.rand)
         t = self.t_pvalue(rand)
-        self.assertEqual(t > 0.05, True, 
-            'mean test for gaussian random number generator (Marsaglia): %f'
-            % t)
+        self.assertGreaterEqual(t, 0.05, 
+            'mean test for gaussian RNG (Marsaglia): %f' % t)
 
     def test_t_boxmuller(self):
         rand = rt.boxmuller(self.rand)
         t = self.t_pvalue(rand)
-        self.assertEqual(t > 0.05, True, 
-            'mean test for gaussian random number generator (B&M): %f'
-            % t)
+        self.assertGreaterEqual(t, 0.05, 
+            'mean test for gaussian RNG (B&M): %f' % t)
 
     def test_t_clt(self):
         rand = rt.clt(self.rand)
         t = self.t_pvalue(rand)
-        self.assertEqual(t > 0.05, True, 
-            'mean test for gaussian random number generator (CLT): %f'
-            % t)
+        self.assertGreaterEqual(t, 0.05, 
+            'mean test for gaussian RNG (CLT): %f' % t)
 
-    def test_t_clt2(self):
-        rand = rt.clt(self.rand, N=5)
+    def test_t_clt_small_sample(self):
+        rand = rt.clt(self.rand, N=3)
         t = self.t_pvalue(rand)
-        self.assertEqual(t > 0.05, True, 
-            'mean test for gaussian random number generator (CLT): %f'
-            % t)
-        rand = rt.clt(self.rand, N=10)
-        t = self.t_pvalue(rand)
-        self.assertEqual(t > 0.05, True, 
-            'mean test for gaussian random number generator (CLT): %f'
-            % t)
-        rand = rt.clt(self.rand, N=20)
-        t = self.t_pvalue(rand)
-        self.assertEqual(t > 0.05, True, 
-            'mean test for gaussian random number generator (CLT): %f'
-            % t)
+        self.assertGreaterEqual(t, 0.05, 
+            'mean test for gaussian RNG (CLT small-sample): %f' % t)
 
     def test_t_exponential(self):
         rand = rt.exponential(self.rand, 2.0)
         t = self.t_pvalue(rand, 0.5)
-        self.assertEqual(t > 0.05, True, 
-            'mean test for exponential random number generator (Exponential): %f'
-            % t)
+        self.assertGreaterEqual(t, 0.05, 
+            'mean test for exponential RNG (Exponential): %f' % t)
 
     def test_t_gamma(self):
         rand = rt.gamma(self.rand)
         t = self.t_pvalue(rand, 1.0)
-        self.assertEqual(t > 0.05, True, 
-            'mean test for gaussian random number generator (Gamma): %f'
-            % t)
+        self.assertGreaterEqual(t, 0.05, 
+            'mean test for gaussian RNG (Gamma): %f' % t)
 
     def test_ks_exponential(self):
         rand = rt.exponential(self.rand, 1.0)
-        ks = self.ks_pvalue(rand, 'expon')
-        self.assert_(ks > 0.05,
-            'ks test for exponential random number generator (Exponential): %f'
-            % ks)
+        t = self.ks_pvalue(rand, 'expon')
+        self.assertGreaterEqual(t, 0.05,
+            'ks test for exponential RNG (Exponential): %f' % t)
 
     def test_ks_clt(self):
         rand = rt.clt(self.rand)
-        ks = self.ks_pvalue(rand)
-        self.assert_(ks > 0.05,
-            'ks test for gaussian random number generator (CLT): %f'
-            % ks)
+        t = self.ks_pvalue(rand)
+        self.assertGreaterEqual(t, 0.05,
+            'ks test for gaussian RNG (CLT): %f' % t)
 
-    def test_ks_clt2(self):
+    def test_ks_clt_small_sample(self):
         rand = rt.clt(self.rand, N=3)
-        ks = self.ks_pvalue(rand)
-        self.assert_(ks > 0.05,
-            'ks test for gaussian random number generator (CLT): %f'
-            % ks)
+        t = self.ks_pvalue(rand)
+        self.assertGreaterEqual(t, 0.05,
+            'ks test for gaussian RNG (CLT small-sample): %f' % t)
 
     def test_ks_boxmuller(self):
         rand = rt.boxmuller(self.rand)
-        ks = self.ks_pvalue(rand)
-        self.assert_(ks > 0.05,
-            'ks test for gaussian random number generator (B&M): %f'
-            % ks)
+        t = self.ks_pvalue(rand)
+        self.assertGreaterEqual(t, 0.05,
+            'ks test for gaussian RNG (B&M): %f' % t)
 
     def test_ks_marsaglia(self):
         rand = rt.marsaglia(self.rand)
-        ks = self.ks_pvalue(rand)
-        self.assert_(ks > 0.05,
-            'ks test for gaussian random number generator (Marsaglia): %f'
-            % ks)
+        t = self.ks_pvalue(rand)
+        self.assertGreaterEqual(t, 0.05,
+            'ks test for gaussian RNG (Marsaglia): %f' % t)
 
     def test_ks_moro(self):
         rand = rt.moro(self.rand)
-        ks = self.ks_pvalue(rand)
-        self.assert_(ks > 0.05,
-            'ks test for gaussian random number generator (Moro): %f'
-            % ks)
+        t = self.ks_pvalue(rand)
+        self.assertGreaterEqual(t, 0.05,
+            'ks test for gaussian RNG (Moro): %f' % t)
 
-    def test_jb_clt2(self):
+    def test_jb_clt_small_sample(self):
         rand = rt.clt(self.rand, N=10)
-        jb = self.jb_pvalue(rand)
-        self.assert_(jb > 0.05,
-            'Jarque-Bera test for gaussian random number generator (CLT): %f' % jb)
+        t = self.jb_pvalue(rand)
+        self.assertGreaterEqual(t, 0.05,
+            'Jarque-Bera test for gaussian RNG (CLT small-sample): %f' % t)
 
     def test_jb_boxmuller(self):
         rand = rt.boxmuller(self.rand)
-        jb = self.jb_pvalue(rand)
-        self.assert_(jb > 0.05,
-            'Jarque-Bera test for gaussian random number generator (B&M) %f' % jb)
+        t = self.jb_pvalue(rand)
+        self.assertGreaterEqual(t, 0.05,
+            'Jarque-Bera test for gaussian RNG (B&M) %f' % t)
 
     def test_jb_marsaglia(self):
         rand = rt.marsaglia(self.rand)
-        jb = self.jb_pvalue(rand)
-        self.assert_(jb > 0.05,
-            'Jarque-Bera test for gaussian random number generator (Marsaglia) %f' % jb)
+        t = self.jb_pvalue(rand)
+        self.assertGreaterEqual(t, 0.05,
+            'Jarque-Bera test for gaussian RNG (Marsaglia) %f' % t)
 
     def test_jb_moro(self):
         rand = rt.moro(self.rand)
-        jb = self.jb_pvalue(rand)
-        self.assert_(jb > 0.05,
-            'Jarque-Bera test for gaussian random number generator (Moro) %f' % jb)
+        t = self.jb_pvalue(rand)
+        self.assertGreaterEqual(t, 0.05,
+            'Jarque-Bera test for gaussian RNG (Moro) %f' % t)
 
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
